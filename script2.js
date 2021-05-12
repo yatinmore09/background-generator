27c3d119c8c3ebb0c645c8df10663673487448cb
yatinmore <yatinmore97@gmail.com>
2021-05-12 20:34:12 +0530

    changing style component

---
+++ script2.js
@@ -0,0 +1,38 @@
+// Advanced Functions
+
+// const array = [1, 2, 3, 5];
+
+// const double_array = [];
+// const newArray = array.forEach((element) => {
+//   double_array.push(element * 2);
+// });
+
+// console.log("foEach", double_array);
+
+// //map
+
+// const mapArray = array.map((num) => {
+//   return num * 2;
+// });
+
+// console.log("map ", mapArray);
+
+// //filter
+
+// const filterArray = array.filter((num) => {
+//   return num > 2;
+// });
+
+// console.log("filter", filterArray);
+
+// // reduce
+
+// const reduceArray = array.reduce((accumulator, num) => {
+//   return accumulator + num;
+// }, 0);
+
+// console.log(reduceArray);
+
+// ------------------------------------
+
+// Advanced Objects
